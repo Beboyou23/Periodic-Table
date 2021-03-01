67b31567423fbdb202830b2376aa7e103613f7c3
Jordan Rarrat <Jrarrat@bsd220.org>
2021-02-28 20:34:50 -0600

    Initial Commit

---
+++ Periodic Table/Periodic_TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Periodic_TableApp.swift
+//  Periodic Table
+//
+//  Created by Student on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Periodic_TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
